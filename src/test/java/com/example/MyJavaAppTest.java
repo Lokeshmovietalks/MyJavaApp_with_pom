a1a151b4f75316a138e417345a31589b56ae01a7
Lokesh <paspuletireddylokesh@gmail.com>
2024-09-30 14:40:50 +0530

    New folder

---
+++ src/test/java/com/example/MyJavaAppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyJavaAppTest {
+
+    @Test
+    void testMain() {
+        assertTrue(true);
+    }
+}
